a523aa9f2b4838ad6a69a008cec19bbb047a83af
Brian-big <briannbig@gmail.com>
2021-10-27 06:19:19 +0300

    code points

---
+++ smileys.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var smile rune = 128515
+	var pi rune = 960
+	var alpha rune = 940
+	var omega rune = 969
+	var bang byte = 33
+	fmt.Printf("%c %c %c %c %c %c \n", smile, pi, alpha, omega, bang)
+}
